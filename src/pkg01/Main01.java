6fe14837599216e38f6dcecc95cc7ae010836337
levananhtu <levananhtu00@gmail.com>
2019-10-03 23:43:45 +0700

    commit03 tb01

--- src/pkg01/Main01.java
+++ src/pkg01/Main01.java
@@ -3,5 +3,6 @@ package pkg01;
 public class Main01 {
     public static void main(String[] args) {
         System.out.println("Hello from branch tb01 1st time");
+        System.out.println("Hello from branch tb01 2nd time");
     }
 }
