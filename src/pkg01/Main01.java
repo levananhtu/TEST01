3f3603bc449fefc94ac69ede0bd4c319ea73ad76
levananhtu <levananhtu00@gmail.com>
2019-10-03 23:48:30 +0700

    Revert "commit04 tb01"
    This reverts commit 69b2033c66c0d42506cbe82217af871f64a08381.

--- src/pkg01/Main01.java
+++ src/pkg01/Main01.java
@@ -4,6 +4,5 @@ public class Main01 {
     public static void main(String[] args) {
         System.out.println("Hello from branch tb01 1st time");
         System.out.println("Hello from branch tb01 2nd time");
-        System.out.println("Hello from branch tb01 3rd time");
     }
 }
