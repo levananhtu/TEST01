14b7a14920488d8cd04e7dace78d54eb487c0bde
dvs-tulva <56066024+dvs-tulva@users.noreply.github.com>
2019-10-04 11:45:17 +0700

    commit02 tb05

--- src/pkg05/Main05.java
+++ src/pkg05/Main05.java
@@ -3,5 +3,6 @@ package pkg05;
 public class Main05 {
     public static void main(String[] args) {
         System.out.println("Hello from branch tb05 1st time");
+        System.out.println("Hello from branch tb05 2nd time");
     }
 }
