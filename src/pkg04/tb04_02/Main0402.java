8064344f85b56daa1dc38f62f2742c627df07081
dvs-tulva <56066024+dvs-tulva@users.noreply.github.com>
2019-10-04 10:43:40 +0700

    commit01 tb04.01

--- src/pkg04/tb04_02/Main0402.java
+++ src/pkg04/tb04_02/Main0402.java
@@ -1,9 +1,7 @@
-package pkg04;
+package pkg04.tb04_02;
 
-public class Main04 {
+public class Main0402 {
     public static void main(String[] args) {
-        System.out.println("Hello from branch tb04 1st time");
-        //
         System.out.println("Hello from branch tb04.01 1st time");
     }
 }
