34e6e5c34aa0ea7eb543a87f06952f48c7297178
levananhtu <levananhtu00@gmail.com>
2019-10-06 21:54:29 +0700

    commit01 tb06.01 rebase

--- src/pkg06/Main06.java
+++ src/pkg06/Main06.java
@@ -3,6 +3,7 @@ package pkg06;
 public class Main06 {
     public static void main(String[] args) {
         System.out.println("Hello from branch tb06 1st time");
+        System.out.println("Hello from branch tb06.01 1st time");
         System.out.println("Hello from branch tb06 2nd time");
     }
 }
---
+++ src/pkg06/tb06_01/Main0601.java
@@ -0,0 +1,7 @@
+package pkg06.tb06_01;
+
+public class Main0601 {
+    public static void main(String[] args) {
+        System.out.println("Hello from branch tb06.01 1st time");
+    }
+}
