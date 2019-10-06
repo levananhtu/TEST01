79fd6e17472983333f0b04ec79b13a089dbc7f1d
levananhtu <levananhtu00@gmail.com>
2019-10-07 00:51:14 +0700

    commit02 tb07

--- src/pkg07/Main07.java
+++ src/pkg07/Main07.java
@@ -3,5 +3,6 @@ package pkg07;
 public class Main07 {
     public static void main(String[] args) {
         System.out.println("Hello from branch tb07 1st time");
+        System.out.println("Hello from branch tb07 2nd time");
     }
 }
